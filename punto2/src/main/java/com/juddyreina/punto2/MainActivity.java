3097f57f55df69bf22cdf931011713bd4c8d6394
jreina <jreina.jk@gmail.com>
2015-09-20 23:03:11 -0500

    practica de moviles

--- punto2/src/main/java/com/juddyreina/punto2/MainActivity.java
+++ punto2/src/main/java/com/juddyreina/punto2/MainActivity.java
@@ -4,6 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +16,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText numero1 = (EditText) findViewById(R.id.num1);
+        final EditText numero2 = (EditText) findViewById(R.id.num2);
+        final TextView messError=(TextView) findViewById(R.id.mess);
+        final TextView messResul=(TextView) findViewById(R.id.result);
+        Button calcular = (Button) findViewById(R.id.calc);
+        final RadioGroup opp= (RadioGroup) findViewById(R.id.operaciones);
+
+        calcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selectedId = opp.getCheckedRadioButtonId();
+                double val1=Double.parseDouble(numero1.getText().toString());
+                double val2=Double.parseDouble(numero2.getText().toString());
+
+                switch(selectedId){
+                    case R.id.sum:
+
+                        double r1=val1+val2;
+                        messError.setText("Resultado: ");
+                        messResul.setText(String.valueOf(r1));
+                        break;
+                    case R.id.rest:
+                        double r2=val1-val2;
+                        messError.setText("Resultado: ");
+                        messResul.setText(String.valueOf(r2));
+                        break;
+                    case R.id.mul:
+                        double r3=val1*val2;
+                        messError.setText("Resultado: ");
+                        messResul.setText(String.valueOf(r3));
+                        break;
+                    case R.id.div:
+                        double r4 = val1 / val2;
+                        if(val2==0){
+                            messError.setText("*******DIVISION POR CERO********");
+                            messResul.setText(String.valueOf(r4));
+                        }
+                        else {
+                            messResul.setText(String.valueOf(r4));
+                        }
+                        break;
+                }
+            }
+        });
     }
 
     @Override
